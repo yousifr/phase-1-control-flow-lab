ed3523c868e0ec6b31619b0d2ec9702ead3af6dc
yousif <yousifr.ismail@gmail.com>
2022-10-24 19:56:26 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet<=400){
+    return "This one is on me"
+  }
+  else if(feet>400){
+    return "You have to pay"
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(feet){
   // Write your code here!
+  return feet<=400?"This one is on me": "you have to pay"
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+console.log(ternaryCheckCity(700))
